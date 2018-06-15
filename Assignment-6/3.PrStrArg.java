d19a8737d75527bfb723ccbde73dc87a2ba7c049
hv <32487524+harshitverma20@users.noreply.github.com>
2018-06-15 20:25:47 +0530

    Assignment-6

---
+++ Assignment-6/3.PrStrArg.java
@@ -0,0 +1,9 @@
+class PrStrArg{
+	public static void main(String s[]){
+		
+		int a = Integer.parseInt(s[0]);
+		int b = Integer.parseInt(s[1]);
+		
+		System.out.println("\n sum is : " +(a+b));
+	}
+}
