225db70e662dd327ed2d9a59c5ca44ea9183be3c
hv <32487524+harshitverma20@users.noreply.github.com>
2018-06-19 16:47:47 +0530

    update 1.RevStr.java

--- Assignment-7/1.RevStr.java
+++ Assignment-7/1.RevStr.java
@@ -1,14 +1,27 @@
 class RevStr{
 	public static void main(String s[]){
-		String str = "abcd";
-	//	String rev = "";
-	/*	for( int i = str.length()-1 ; i >= 0 ; i--){
-			System.out.print(str.charAt(i));
-		}
-	*/
-		for ( int i=str.length()-1 ;i>=0 ; i--)
-			String rev = String.valueof(str.charAt(i));
 		
-		System.out.println(rev);
-	}
+		StringBuffer str = new StringBuffer("!!weivdacA olleH!!");
+        	str.reverse();
+        	System.out.print("Reverse of string is : ");
+        	System.out.println(str);
+		
+		// OR
+		
+		/*
+		String str1 = "Hello Acadview!!";
+		System.out.print("\n Reverse of string is : ");
+			for( int i = str1.length()-1 ; i >= 0 ; i--){
+				System.out.print(str1.charAt(i));
+		*/		
+		// OR
+		
+		/*
+		String str = "abcd";
+        	String rev = "";
+        		for ( int i=str.length()-1 ;i>=0 ; i--)
+            			rev = rev+String.valueOf(str.charAt(i));
+		
+            	System.out.println(rev);
+		*/
 }
